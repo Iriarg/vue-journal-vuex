3e537d090185bd6d29ae1640e9bc685efb0c9466
Iria <iriargon94@gmail.com>
2023-01-15 23:02:15 +0100

    update done

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -40,13 +40,14 @@
 
     <Fab 
         icon="fa-save"
+        @on:click="saveEntry"
     />
 
 </template>
 
 <script>
 import { defineAsyncComponent } from 'vue'
-import { mapGetters } from 'vuex' // computed!!!
+import { mapGetters, mapActions } from 'vuex' 
 
 import getDayMonthYear from '../helpers/getDayMonthYear'
 
@@ -84,11 +85,18 @@ export default {
     },
 
     methods: {
+        ...mapActions('journal', ['updateEntry']),
+
         loadEntry() {
             const entry = this.getEntryById( this.id )
             if ( !entry ) return this.$router.push({ name: 'no-entry' })
 
             this.entry = entry
+        },
+        async saveEntry() {
+            console.log('Guardando entrada')
+
+            this.updateEntry(this.entry)
         }
     },
 
