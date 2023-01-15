e5bd6908ba3dd2d47a152ae21325227a63718a85
Iria <iriargon94@gmail.com>
2023-01-15 20:07:42 +0100

    add git

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,26 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString,
+            text: 'Est mollit dolor qui dolor culpa quis. Excepteur reprehenderit deserunt qui amet labore occaecat do labore velit quis elit pariatur veniam cupidatat. Dolore amet do cupidatat ea laborum nisi consequat. Est voluptate commodo veniam voluptate occaecat elit incididunt labore. Lorem quis amet eiusmod magna. Reprehenderit consectetur Lorem commodo dolore proident est eiusmod sint cupidatat.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString,
+            text: 'Aute excepteur id irure esse culpa tempor nisi labore do ad consequat in do. Cupidatat enim aute nulla ex nostrud minim aliquip do velit minim irure id. Velit do reprehenderit mollit esse anim culpa officia adipisicing et ipsum et laboris aliqua commodo. Commodo nisi duis mollit sint tempor consequat cillum.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString,
+            text: 'Consequat nostrud irure aliquip reprehenderit nulla anim magna quis velit ipsum ad culpa duis. Esse incididunt adipisicing adipisicing excepteur sint. Nisi eu veniam anim labore minim occaecat commodo non aliqua ipsum commodo et.',
+            picture: null,
+        }
+
+
+    ]
+})
