7241f3c54eead3feb4f3ab36e8cdd451f0b29790
Iria <iriargon94@gmail.com>
2023-01-15 14:01:15 +0100

    until plus button

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+  class="entry-container mb-3 pointer p-2">
+    <div class="entry-title d-flex">
+        <span class="text-success fs-5 d-flex">15</span>
+        <span class="mx-1 fs-5">Julio</span>
+        <span class="mx-2 fw-light">2021, jueves</span>
+    </div>
+
+    <div class="entry-description">
+        Amet veniam amet nulla enim est enim ut. Ut ea eu eiusmod ullamco cillum sit proident ex sit anim laborum non qui. Velit non occaecat enim ipsum incididunt minim et irure et. Occaecat ad esse dolor cillum.
+    </div>
+
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+}
+</style>
