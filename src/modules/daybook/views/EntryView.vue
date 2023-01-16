66a4e4a775391c2a6c1ce209097bbc7d6b6b233a
Iria <iriargon94@gmail.com>
2023-01-16 01:40:04 +0100

    CRUD done

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -10,12 +10,22 @@
             </div>
 
             <div>
-                <button class="btn btn-danger mx-2">
+
+                <input type="file"
+                    @change="onSelectedImage"
+                    ref="imageSelector"
+                    v-show="false">
+
+                <button 
+                    v-if="entry.id"
+                    class="btn btn-danger mx-2"
+                    @click="onDeleteEntry">
                     Borrar
                     <i class="fa fa-trash-alt"></i>
                 </button>
 
-                <button class="btn btn-primary">
+                <button class="btn btn-primary"
+                    @click="onSelectImage">
                     Subir foto
                     <i class="fa fa-upload"></i>
                 </button>
@@ -31,10 +41,18 @@
         </div>
 
 
-        <img 
-            src="https://www.robertlandscapes.com/wp-content/uploads/2014/11/landscape-322100_1280.jpg" 
+        <img
+            v-if="entry.picture && !localImage"
+            :src="entry.picture"
+            alt="entry-picture"
+            class="img-thumbnail"> 
+
+        <img
+            v-if="localImage"
+            :src="localImage"
             alt="entry-picture"
             class="img-thumbnail">
+
 
     </template>
 
@@ -48,8 +66,10 @@
 <script>
 import { defineAsyncComponent } from 'vue'
 import { mapGetters, mapActions } from 'vuex' 
+import Swal from 'sweetalert2'
 
 import getDayMonthYear from '../helpers/getDayMonthYear'
+import uploadImage from '../helpers/uploadImage'
 
 export default {
     props: {
@@ -64,7 +84,9 @@ export default {
 
     data() {
         return {
-            entry: null
+            entry: null,
+            localImage: null,
+            file: null
         }
     },
 
@@ -85,18 +107,91 @@ export default {
     },
 
     methods: {
-        ...mapActions('journal', ['updateEntry']),
+        ...mapActions('journal', ['updateEntry', 'createEntry', 'deleteEntry']),
 
         loadEntry() {
-            const entry = this.getEntryById( this.id )
-            if ( !entry ) return this.$router.push({ name: 'no-entry' })
+
+            let entry;
+
+            if ( this.id === 'new') {
+                entry = {
+                    text: '',
+                    date: new Date().getTime()
+                }
+            } else {
+                entry = this.getEntryById( this.id )
+                if ( !entry ) return this.$router.push({ name: 'no-entry' })
+
+            }
 
             this.entry = entry
         },
-        async saveEntry() {
-            console.log('Guardando entrada')
 
-            this.updateEntry(this.entry)
+        async saveEntry() {
+
+            new Swal({
+                title: 'Espere por favor',
+                allowOutsideClick: false
+            })
+            Swal.showLoading()
+
+            const picture = await uploadImage(this.file)
+
+            this.entry.picture = picture
+
+
+            if(this.entry.id){
+                await this.updateEntry(this.entry)
+            } else {
+                const id = await this.createEntry(this.entry)
+            
+                this.$router.push({name: 'entry', params: {id}})
+            }
+
+            this.file = null
+            Swal.fire('Guardado', 'Entrada guardada con éxito!', 'success')
+        
+        },
+
+        async onDeleteEntry() {
+
+            const {isConfirmed} = await Swal.fire({
+                title: '¿Está seguro?',
+                text: 'Una vez borrado no se podrá recuperar.',
+                showDenyButton: true,
+                confirmButtonText: 'Borrar definitivamente'
+            })
+
+            if (isConfirmed) {
+                new Swal({
+                    title:'Espere por favor',
+                    allowOutsideClick: false
+                })
+                Swal.showLoading()
+                await this.deleteEntry(this.entry.id)
+                this.$router.push({name: 'no-entry'})
+
+                Swal.fire('Eliminado','','success')
+            }
+        },
+
+        onSelectedImage (event) {
+            const file = event.target.files[0]
+            if (!file) {
+                this.localImage = null
+                this.file = null
+                return
+            }
+
+            this.file = file
+
+            const fr = new FileReader()
+            fr.onload = () => this.localImage = fr.result
+            fr.readAsDataURL(file)
+        },
+
+        onSelectImage() {
+            this.$refs.imageSelector.click()
         }
     },
 
